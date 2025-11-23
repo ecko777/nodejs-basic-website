14809d1fba460957c75802304c45f697f9c0b7c0
Gerardo <ferbat.sor@gmail.com>
2025-11-23 08:18:08 +0100

    fix(eslint): Use 'overrides' to scope Jest environment, resolving no-undef error

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,24 +1,34 @@
 module.exports = {
-  env: {
-    node: true,
-    es2021: true,
-    jest: true  
-  },
+  // Configuración base para todo el proyecto (archivos JS normales)
   extends: [
     "eslint:recommended", 
-    "plugin:prettier/recommended",
-    "plugin:jest/recommended" 
+    "plugin:prettier/recommended"
   ],
   parserOptions: {
     ecmaVersion: "latest",
     sourceType: "module"
   },
-  plugins: [
-    "jest" 
-  ],
+  env: {
+    node: true,
+    es2021: true
+  },
   rules: {
     "no-console": "off",
     "no-unused-vars": ["warn", { "argsIgnorePattern": "^_" }],
     "consistent-return": "warn"
-  }
+  },
+
+ 
+  overrides: [
+    {
+      files: ["**/__tests__/**/*.js"],
+      env: {
+        jest: true // Habilita las variables globales de Jest (describe, test, expect)
+      },
+      extends: [
+        "plugin:jest/recommended" // Carga las reglas de Jest 
+      ],
+      plugins: ["jest"]
+    }
+  ]
 };
