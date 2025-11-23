9d8b84a948a6ef79638a791f64ba7ac8530fef0b
Gerardo <ferbat.sor@gmail.com>
2025-11-23 07:02:46 +0100

    test: Add basic Jest setup test in __tests__ directory

---
+++ __tests__/app.test.js
@@ -0,0 +1,7 @@
+// app.test.js
+describe('Verificación de Pruebas', () => {
+  test('Debe poder ejecutar una prueba simple', () => {
+    // Esto asegura que Jest se haya cargado correctamente.
+    expect(1 + 1).toBe(2);
+  });
+});
